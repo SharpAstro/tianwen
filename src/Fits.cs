7877308a5925da0363ec40ae4e3a1f7b80f2c3d5
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-05-05 14:52:37 +1000

    rename to avoid collision with FITS

--- src/Fits.cs
+++ src/Fits.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Astap.Lib
+﻿namespace Astap.Lib
 {
-    public static class Fits
+    public static class Analyse
     {
-        public static (int stars, double medianHFD, double medianFWHM, double background) Analyse(string file, double snr = 10.0, int maxStars = 500)
+        public static (int stars, double medianHFD, double medianFWHM, double background) AnalyseFITS(string file, double snr = 10.0, int maxStars = 500)
         {
             var stars = Native.analyse_fits(file, snr, maxStars, out var medianHFD, out var medianFWHM, out var background);
 
