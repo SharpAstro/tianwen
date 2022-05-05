731c3071325515b7a335b4c324636f740fd78e43
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-05-05 14:44:56 +1000

    Add public Fits interface

--- src/Native.cs
+++ src/Native.cs
@@ -1,30 +1,9 @@
 using System.Runtime.InteropServices;
+using static ZWOptical.ASISDK.ASICameraDll2;
 
 namespace Astap.Lib
 {
-    public enum ASI_ERROR_CODE
-    {
-        SUCCESS = 0,
-        INVALID_INDEX, //no camera connected or index value out of boundary
-        INVALID_ID, //invalid ID
-        INVALID_CONTROL_TYPE, //invalid control type
-        CAMERA_CLOSED, //camera didn't open
-        CAMERA_REMOVED, //failed to find the camera, maybe the camera has been removed
-        INVALID_PATH, //cannot find the path of the file
-        INVALID_FILEFORMAT,
-        INVALID_SIZE, //wrong video format size
-        INVALID_IMGTYPE, //unsupported image formate
-        OUTOF_BOUNDARY, //the startpos is out of boundary
-        TIMEOUT, //timeout
-        INVALID_SEQUENCE,//stop capture first
-        BUFFER_TOO_SMALL, //buffer size is not big enough
-        VIDEO_MODE_ACTIVE,
-        EXPOSURE_IN_PROGRESS,
-        GENERAL_ERROR,//general error, eg: value is out of valid range
-        END
-    }
-
-    internal static class AstapLib
+    internal static class Native
     {
         const string astap_lib = "astap_lib";
 
