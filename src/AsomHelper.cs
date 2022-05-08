47264f993d3e6ab05ab55d336d59c990942d3559
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-05-08 14:48:46 +1000

    Add ASCOM profile

---
+++ src/AsomHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Astap.Lib
+{
+    public static class AsomHelper
+    {
+        public static dynamic NewComObject(string progId) => Type.GetTypeFromProgID(progId) is Type type ? Activator.CreateInstance(type) : null as dynamic;
+
+        public static IEnumerable<string> EnumerateArrayList(dynamic list) => list is ArrayList arrayList ? arrayList.Cast<string>() : Array.Empty<string>();
+    }
+}
