f4e154c7eae44e9043fe39c37da5b6dee8d20920
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-05-08 14:48:46 +1000

    Add ASCOM profile

---
+++ src/AscomProfile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using static Astap.Lib.AsomHelper;
+
+namespace Astap.Lib
+{
+    public class AscomProfile : IDisposable
+    {
+        private readonly dynamic _profile;
+        private bool disposedValue;
+
+        public AscomProfile() => _profile = NewComObject("ASCOM.Utilities.Profile");
+
+        public IEnumerable<string> RegisteredDeviceTypes => EnumerateArrayList(_profile?.RegisteredDeviceTypes);
+
+        public IEnumerable<string> RegisteredDevices(string deviceType) => EnumerateArrayList(_profile?.RegisteredDevices(deviceType));
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _profile?.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
