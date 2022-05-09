c4ef2e0b0d429f62f114235d02f4bb87f2f88031
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-05-09 12:12:48 +1000

    Use base class

--- src/AscomProfile.cs
+++ src/AscomProfile.cs
@@ -2,41 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using static Astap.Lib.AsomHelper;
 
 namespace Astap.Lib;
 
-public class AscomProfile : IDisposable
+public class AscomProfile : AscomBase
 {
-    private readonly dynamic _profile;
-    private bool disposedValue;
+    public AscomProfile() : base("ASCOM.Utilities.Profile") { }
 
-    public AscomProfile() => _profile = NewComObject("ASCOM.Utilities.Profile");
+    public IEnumerable<string> RegisteredDeviceTypes => EnumerateProperty<string>(_comObject?.RegisteredDeviceTypes);
 
-    public IEnumerable<string> RegisteredDeviceTypes => EnumerateProperty<string>(_profile?.RegisteredDeviceTypes);
-
-    public IEnumerable<(string progId, string displayName)> RegisteredDevices(string deviceType) => EnumerateKeyValueProperty(_profile?.RegisteredDevices(deviceType));
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (!disposedValue)
-        {
-            if (disposing)
-            {
-                if (_profile is IDisposable disposable)
-                {
-                    disposable.Dispose();
-                }
-            }
-
-            disposedValue = true;
-        }
-    }
-
-    public void Dispose()
-    {
-        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        Dispose(disposing: true);
-        GC.SuppressFinalize(this);
-    }
+    public IEnumerable<(string progId, string displayName)> RegisteredDevices(string deviceType) => EnumerateKeyValueProperty(_comObject?.RegisteredDevices(deviceType));
 }
