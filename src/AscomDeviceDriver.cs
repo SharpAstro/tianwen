3dc7a840b9cc4e68ea093ed424cf133241f2decc
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-05-09 14:35:54 +1000

    Add device base

---
+++ src/AscomDeviceDriver.cs
@@ -0,0 +1,20 @@
+﻿namespace Astap.Lib;
+
+public class AscomDeviceDriver : AscomBase
+{
+    private readonly AscomDevice _device;
+
+    public AscomDeviceDriver(AscomDevice device) : base(device.ProgId) => _device = device;
+
+    public string Name => _comObject?.Name ?? _device.DisplayName;
+
+    public string? Description => _comObject?.Description as string;
+
+    public string? DriverInfo => _comObject?.DriverInfo as string;
+
+    public string? DriverVersion => _comObject?.DriverVersion as string;
+
+    public string DriverType => _device.DeviceType;
+
+    public void SetupDialog() => _comObject?.SetupDialog();
+}
