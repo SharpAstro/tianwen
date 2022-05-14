43e9a324f1e9bc64f962d52499c90831ef357da6
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-05-14 17:26:20 +1000

    add profile

--- src/Devices/DeviceBase.cs
+++ src/Devices/DeviceBase.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Reflection;
 using System.Web;
 
@@ -7,39 +9,39 @@ namespace Astap.Lib.Devices;
 
 public abstract record class DeviceBase(Uri DeviceUri)
 {
+    protected const string UriScheme = "device";
+
     public string DeviceType => HttpUtility.HtmlDecode(DeviceUri.Fragment.TrimStart('#'));
 
     public string DeviceId => string.Join('/', DeviceUri.Segments[1..]);
 
-    public string DisplayName => HttpUtility.ParseQueryString(DeviceUri.Query)["displayName"] ?? "";
+    private NameValueCollection? _query;
+    protected NameValueCollection Query => _query ??= HttpUtility.ParseQueryString(DeviceUri.Query);
+
+    public string DisplayName => Query["displayName"] ?? "";
 
     public string DeviceClass => DeviceUri.Host;
 
     public static bool TryFromUri(Uri deviceUri, [NotNullWhen(true)] out DeviceBase? device)
     {
-        if (deviceUri.Scheme == "device")
+        if (deviceUri.Scheme != UriScheme)
         {
-            foreach (var assembly in new[] { typeof(DeviceBase).Assembly, Assembly.GetCallingAssembly(), Assembly.GetEntryAssembly() })
-            {
-                if (assembly is null)
-                {
-                    continue;
-                }
-                foreach (var exported in assembly.GetExportedTypes())
-                {
-                    if (string.Equals(exported.Name, deviceUri.Host, StringComparison.OrdinalIgnoreCase)
-                        && exported.IsSubclassOf(typeof(DeviceBase))
-                        && exported.GetConstructor(new[] { typeof(Uri) }) is ConstructorInfo uriConstructor
-                        && uriConstructor.Invoke(new[] { deviceUri }) is DeviceBase newDevice)
-                    {
-                        device = newDevice;
-                        return true;
-                    }
-                }
-            }
+            device = null;
+            return false;
         }
 
-        device = null;
-        return false;
+        var findDeviceInSubclass =
+            from assembly in new[] { typeof(DeviceBase).Assembly, Assembly.GetCallingAssembly(), Assembly.GetEntryAssembly() }
+            where assembly is not null
+            from exported in assembly.GetExportedTypes()
+            where exported.Name.Equals(deviceUri.Host, StringComparison.OrdinalIgnoreCase) && exported.IsSubclassOf(typeof(DeviceBase))
+            let constructor = exported.GetConstructor(new[] { typeof(Uri) })
+            where constructor is not null
+            let obj = constructor.Invoke(new[] { deviceUri }) as DeviceBase
+            where obj is not null
+            select obj;
+
+        device = findDeviceInSubclass.FirstOrDefault();
+        return device is not null;
     }
 }
