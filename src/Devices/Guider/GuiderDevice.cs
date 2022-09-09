1504536e7a36a2eab38d5260f0f2868864e0fb98
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-09-10 09:39:03 +1000

    Implement device source for pHD2 guider

--- src/Devices/Guider/GuiderDevice.cs
+++ src/Devices/Guider/GuiderDevice.cs
@@ -4,8 +4,6 @@ namespace Astap.Lib.Devices.Guider
 {
     public record class GuiderDevice(Uri DeviceUri) : DeviceBase(DeviceUri)
     {
-        internal const string GuiderDeviceType = "Guider";
-
         public GuiderDevice(string deviceType, string deviceId, string displayName)
             : this(new Uri($"{UriScheme}://{typeof(GuiderDevice).Name}/{deviceId}?displayName={displayName}#{deviceType}"))
         {
--- src/Devices/Guider/GuiderDeviceDriverFactory.cs
+++ src/Devices/Guider/GuiderDeviceDriverFactory.cs
@@ -4,15 +4,30 @@ namespace Astap.Lib.Devices.Guider
 {
     public static class GuiderDeviceDriverFactory
     {
-        public static bool TryInstatiateDriver(GuiderDevice device, [NotNullWhen(true)] out IGuider? guider)
+        public static bool TryInstantiateDriver(GuiderDevice device, [NotNullWhen(true)] out IGuider? guider)
+            => TryInstantiate(device, out guider);
+
+        public static bool TryInstantiateDeviceSource(GuiderDevice device, [NotNullWhen(true)] out IDeviceSource<GuiderDevice>? deviceSource)
+            => TryInstantiate(device, out deviceSource);
+
+        private static bool TryInstantiate<T>(GuiderDevice device, [NotNullWhen(true)] out T? iface)
         {
-            guider = device.DeviceType switch
+            var driver = device.DeviceType switch
             {
-                "PHD2" => new PHD2GuiderDriver(device),
+                PHD2GuiderDriver.PHD2 => new PHD2GuiderDriver(device),
                 _ => null
             };
 
-            return guider is not null;
+            if (driver is T asT)
+            {
+                iface = asT;
+                return true;
+            }
+            else
+            {
+                iface = default;
+                return false;
+            }
         }
     }
 }
