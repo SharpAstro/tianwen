9bdfe31f6c46c57c35bfa720494a3cf753a9bf1f
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-09-10 09:13:47 +1000

    Add Guider source

--- src/Devices/Guider/GuiderDevice.cs
+++ src/Devices/Guider/GuiderDevice.cs
@@ -4,6 +4,8 @@ namespace Astap.Lib.Devices.Guider
 {
     public record class GuiderDevice(Uri DeviceUri) : DeviceBase(DeviceUri)
     {
+        internal const string GuiderDeviceType = "Guider";
+
         public GuiderDevice(string deviceType, string deviceId, string displayName)
             : this(new Uri($"{UriScheme}://{typeof(GuiderDevice).Name}/{deviceId}?displayName={displayName}#{deviceType}"))
         {
