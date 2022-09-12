a3f32c4c5b70166a446a35d9e8e68762e5c927f9
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-09-13 07:53:01 +1000

    Add OpenNGC

---
+++ src/Astap.Lib.Tests/OpenNGCReaderTests.cs
@@ -0,0 +1,86 @@
+﻿using Astap.Lib.Astrometry;
+using Shouldly;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Astap.Lib.Tests
+{
+    public class OpenNGCReaderTests
+    {
+        const CatalogIndex IC0715NW = (CatalogIndex)((ulong)'I' << 56 | (ulong)'0' << 48 | (ulong)'7' << 40 | (ulong)'1' << 32 | '5' << 24 | '_' << 16 | 'N' << 8 | 'W');
+
+        [Theory]
+        [InlineData("NGC7293", ObjectType.PlanetaryNebula, (CatalogIndex)((ulong)'N' << 32 | '7' << 24 | '2' << 16 | '9' << 8 | '3'), Constellation.Aquarius)]
+        [InlineData("NGC0056", ObjectType.Other, (CatalogIndex)((ulong)'N' << 32 | '0' << 24 | '0' << 16 | '5' << 8 | '6'), Constellation.Pisces)]
+        [InlineData("IC1000", ObjectType.Galaxy, (CatalogIndex)((ulong)'I' << 32 | '1' << 24 | '0' << 16 | '0' << 8 | '0'), Constellation.Bootes)]
+        [InlineData("IC0715NW", ObjectType.Galaxy, IC0715NW, Constellation.Crater)]
+        public async Task GivenObjectIdWhenLookingItUpThenAnEntryIsReturned(
+            string indexEntry,
+            ObjectType expectedObjType,
+            CatalogIndex expectedCatalogIindex,
+            Constellation expectedConstellation
+        )
+        {
+            // given
+            var reader = new OpenNGCReader();
+            var (actualRead, actualFailed) = await reader.ReadEmbeddedDataAsync();
+
+            // when
+            var found = reader.TryLookupByIndex(indexEntry, out var deepSkyObject);
+
+            // then
+            actualRead.ShouldBeGreaterThan(13000);
+            actualFailed.ShouldBe(0);
+            found.ShouldBeTrue();
+            deepSkyObject.ShouldNotBeNull();
+            deepSkyObject.Index.ShouldBe(expectedCatalogIindex);
+            deepSkyObject.ObjType.ShouldBe(expectedObjType);
+            deepSkyObject.Constellation.ShouldBe(expectedConstellation);
+        }
+
+        [Theory]
+        [InlineData("N11", "N0011")]
+        [InlineData("NGC0011", "N0011")]
+        [InlineData("NC 120", "N0120")]
+        [InlineData("NCG00055", "N0055")]
+        [InlineData("NCGX999", "N0999")]
+        [InlineData("I 999", "I0999")]
+        [InlineData("M12", "M012")]
+        [InlineData(" M12", "M012")]
+        [InlineData("M00013", "M013")]
+        [InlineData("Messier 120", "M120")]
+        [InlineData("IC4473 NED01", "I4473N01")]
+        [InlineData("NGC0526A", "N0526_A")]
+        [InlineData("NGC 0526 B", "N0526_B")]
+        [InlineData("N 0526_C", "N0526_C")]
+        [InlineData("IC0715NW", "I0715_NW")]
+        [InlineData("IC0133S", "I0133_S")]
+        public void GivenAUserInputWhenCleaningItUpThenACleanedupEntryIsReturned(string input, string expectedOutput)
+        {
+            var success = OpenNGCReader.TryGetCleanedUpCatalogName(input, out var actualCleanedUp);
+
+            success.ShouldBeTrue();
+            actualCleanedUp.ShouldNotBeNull();
+            actualCleanedUp.ShouldBe(expectedOutput);
+        }
+
+        [Theory]
+        [InlineData("Not an index")]
+        [InlineData("   ")]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("I am not the index you are looking for")]
+        [InlineData("Maybe not")]
+        [InlineData("U2")]
+        [InlineData("4Sq")]
+        [InlineData("N 0526__")]
+        [InlineData("N 0526 ABC01")]
+        public void GivenAnInvalidUserInputWhenCleaningUpThenNothingIsReturned(string input)
+        {
+            var success = OpenNGCReader.TryGetCleanedUpCatalogName(input, out var actualCleanedUp);
+
+            success.ShouldBeFalse();
+            actualCleanedUp.ShouldBeNull();
+        }
+    }
+}
---
+++ src/Astrometry/OpenNGCReader.cs
@@ -0,0 +1,271 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Astap.Lib.Astrometry
+{
+    public enum ObjectType : ulong
+    {
+        Star = '*',
+        DoubleStar = '*' << 8 | '*',
+        AssociationOfStars = '*' << 24 | 'A' << 16 | 's' << 8 | 's',
+        OpenCluster = 'O' << 16 | 'C' << 8 | 'l',
+        GlobularCluster = 'G' << 16 | 'C' << 8 | 'l',
+        ClusterAndNebula = 'C' << 24 | 'l' << 16 | '+' << 8 | 'N',
+        Galaxy = 'G',
+        GalaxyPair = ((ulong)'G' << 32) | 'P' << 24 | 'a' << 16 | 'i' << 8 | 'r',
+        GalaxyTriplet = ((ulong)'G' << 32) | 'T' << 24 | 'r' << 16 | 'p' << 8 | 'l',
+        GroupOfGalaxies = ((ulong)'G' << 40) | ((ulong)'G' << 32) | 'r' << 24 | 'o' << 16 | 'u' << 8 | 'p',
+        PlanetaryNebula = 'P' << 8 | 'N',
+        HIIRegion = 'H' << 16 | 'I' << 8 | 'I',
+        DarkNebula = 'D' << 24 | 'r' << 16 | 'k' << 8 | 'N',
+        EmissionNebula = 'E' << 16 | 'm' << 8 | 'N',
+        Nebula = 'N' << 16 | 'e' << 8 | 'b',
+        ReflectionNebula = 'R' << 16 | 'f' << 8 | 'N',
+        SupernovaRemnant = 'S' << 16 | 'N'  << 8 | 'R',
+        NovaStar = 'N' << 24 | 'o' << 16 | 'v' << 8 | 'a',
+        NonExistent = ((ulong)'N' << 32) | 'o' << 24 | 'n' << 16 | 'v' << 8 | 'a',
+        Duplicate = 'D' << 16 | 'u' << 8 | 'p',
+        Other = ((ulong)'O' << 32) | 't' << 24 | 'h' << 16 | 'e' << 8 | 'r'
+    }
+
+    public enum Constellation : ulong
+    {
+        Aquarius = 'A' << 16 | 'q' << 8 | 'r',
+        Bootes = 'B' << 16 | 'o' << 8 | 'o',
+        Crater = 'C' << 16 | 'r' << 8 | 't',
+        Pisces = 'P' << 16 | 's' << 8 | 'c',
+        SerpensCaput = 'S' << 16 | 'e' << 8 | '1',
+        SerpensCaudi = 'S' << 16 | 'e' << 8 | '2',
+    }
+
+    /// <summary>
+    /// Represents a unique entry in a catalogue s.th. NGC0001, M13 or IC0001
+    /// </summary>
+    public enum CatalogIndex : ulong { }
+
+    public record DeepSkyObject(CatalogIndex Index, ObjectType ObjType, string RA, string Dec, Constellation Constellation);
+
+    public class OpenNGCReader
+    {
+        const string NGC = nameof(NGC);
+        const string IC = nameof(IC);
+        const string M = nameof(M);
+
+        private readonly Dictionary<CatalogIndex, DeepSkyObject> _objectsByIndex = new(14000);
+        private readonly Dictionary<CatalogIndex, CatalogIndex[]> _crossLookupTable = new(900);
+        private readonly Dictionary<string, CatalogIndex[]> _objectsByCommonName = new(200);
+
+        public OpenNGCReader() { }
+
+        public bool TryLookupByIndex(string name, [NotNullWhen(true)] out DeepSkyObject? deepSkyObject)
+        {
+            if (TryGetCleanedUpCatalogName(name, out var cleanedUp))
+            {
+                return TryLookupByIndex(AbbreviationToEnumMember<CatalogIndex>(cleanedUp), out deepSkyObject);
+            }
+            else
+            {
+                deepSkyObject = default;
+                return false;
+            }
+        }
+
+        private bool TryLookupByIndex(CatalogIndex indexEntry, [NotNullWhen(true)] out DeepSkyObject? deepSkyObject)
+            => _objectsByIndex.TryGetValue(indexEntry, out deepSkyObject);
+
+        public async Task<(int processed, int failed)> ReadEmbeddedDataAsync()
+        {
+            int processed = 0;
+            int failed = 0;
+            var assy = typeof(OpenNGCReader).Assembly;
+            var ngcCSV = assy.GetManifestResourceNames().FirstOrDefault(p => p.EndsWith("NGC.csv"));
+            if (ngcCSV is null || assy.GetManifestResourceStream(ngcCSV) is not Stream ngcCSVStream)
+            {
+                return (processed, failed);
+            }
+
+            using var ngcCSVTextReader = new StreamReader(ngcCSVStream, new UTF8Encoding(false));
+            using var ngcCSVReader = new CsvReader(ngcCSVTextReader, new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ";" });
+
+            if (!await ngcCSVReader.ReadAsync() || !ngcCSVReader.ReadHeader())
+            {
+                return (processed, failed);
+            }
+
+            while (await ngcCSVReader.ReadAsync())
+            {
+                if (ngcCSVReader.TryGetField<string>("Name", out var entryName)
+                    && ngcCSVReader.TryGetField<string>("Type", out var objectTypeAbbr)
+                    && ngcCSVReader.TryGetField<string>("RA", out var raHMS)
+                    && ngcCSVReader.TryGetField<string>("Dec", out var decDMS)
+                    && ngcCSVReader.TryGetField<string>("Const", out var constAbbr)
+                    && TryGetCleanedUpCatalogName(entryName, out var cleanedUpName)
+                )
+                {
+                    var objectType = AbbreviationToEnumMember<ObjectType>(objectTypeAbbr);
+                    var @const = AbbreviationToEnumMember<Constellation>(constAbbr);
+                    var indexEntry = AbbreviationToEnumMember<CatalogIndex>(cleanedUpName);
+                    _objectsByIndex[indexEntry] = new DeepSkyObject(indexEntry, objectType, "", "", @const);
+
+                    if (objectType == ObjectType.Duplicate)
+                    {
+                        if (ngcCSVReader.TryGetField<string>(NGC, out var ngcDupSuffix) && TryGetCleanedUpCatalogName(NGC + ngcDupSuffix, out var ngcDup))
+                        {
+                            AddLookupEntry(_crossLookupTable, indexEntry, AbbreviationToEnumMember<CatalogIndex>(ngcDup));
+                        }
+                        if (ngcCSVReader.TryGetField<string>(IC, out var icDupSuffix) && TryGetCleanedUpCatalogName(IC + icDupSuffix, out var icDup))
+                        {
+                            AddLookupEntry(_crossLookupTable, indexEntry, AbbreviationToEnumMember<CatalogIndex>(icDup));
+                        }
+                    }
+
+                    if (ngcCSVReader.TryGetField<int>(M, out var messierEntry))
+                    {
+                        AddLookupEntry(_crossLookupTable, AbbreviationToEnumMember<CatalogIndex>($"{M[0]}{messierEntry:000}"), indexEntry);
+                    }
+
+                    if (ngcCSVReader.TryGetField<string>("Common names", out var commonNamesEntry))
+                    {
+                        var commonNames = commonNamesEntry.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                        foreach (var commonName in commonNames)
+                        {
+                            AddLookupEntry(_objectsByCommonName, commonName, indexEntry);
+                        }
+                    }
+
+                    processed++;
+                }
+                else
+                {
+                    failed++;
+                }
+            }
+
+            return (processed, failed);
+        }
+
+        private static void AddLookupEntry<T>(Dictionary<T, CatalogIndex[]> lookupTable, T master, CatalogIndex duplicate)
+            where T : notnull
+        {
+            if (!lookupTable.TryAdd(master, new[] { duplicate }))
+            {
+                lookupTable[master] = ResizeAndAdd(lookupTable[master], duplicate);
+            }
+        }
+
+        private static CatalogIndex[] ResizeAndAdd(CatalogIndex[] existing, CatalogIndex indexEntry)
+        {
+            var @new = new CatalogIndex[existing.Length + 1];
+            Array.Copy(existing, @new, existing.Length);
+            @new[^1] = indexEntry;
+            return @new;
+        }
+
+        static readonly Regex ExtendedCatalogEntryPattern = new(@"^(N|I|NGC|IC) \s* ([0-9]{1,4}) \s* (?:(N(?:ED)? \s* ([0-9]{1,2})) | [_]?([A-Z]{1,2}))$",
+            RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace);
+
+        public static bool TryGetCleanedUpCatalogName(string? input, [NotNullWhen(true)] out string? cleanedUp)
+        {
+            var trimmedInput = input?.Trim();
+            if (string.IsNullOrEmpty(trimmedInput))
+            {
+                cleanedUp = default;
+                return false;
+            }
+
+            var (chars, digits) = trimmedInput[0] switch
+            {
+                'N' => (new char[5] { 'N', '0', '0', '0', '0' }, 4),
+                'I' => (new char[5] { 'I', '0', '0', '0', '0' }, 4),
+                'M' => (new char[4] { 'M', '0', '0', '0' }, 3),
+                _ => (Array.Empty<char>(), 0)
+            };
+
+            if (digits <= 0)
+            {
+                cleanedUp = default;
+                return false;
+            }
+
+            // test for slow path
+            var firstDigit = trimmedInput.IndexOfAny(new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
+            if (trimmedInput[0] != 'M' && firstDigit > 0 && trimmedInput.IndexOfAny(new[] { 'A', 'B', 'C', 'D', 'E', 'F', 'S', 'W', 'N' }, firstDigit) > firstDigit)
+            {
+                var match = ExtendedCatalogEntryPattern.Match(trimmedInput);
+                if (match.Success && match.Groups.Count == 6)
+                {
+                    var NorI = match.Groups[1].ValueSpan[0..1].ToString();
+                    var number = match.Groups[2].Value;
+                    if (match.Groups[5].Length == 0)
+                    {
+                        var nedGroupSuffix = match.Groups[4].Value;
+                        cleanedUp = string.Concat(NorI, number, 'N', nedGroupSuffix);
+                        return true;
+                    }
+                    else
+                    {
+                        var letterSuffix = match.Groups[5].Value;
+                        cleanedUp = string.Concat(NorI, number, '_', letterSuffix);
+                        return true;
+                    }
+                }
+                else
+                {
+                    cleanedUp = default;
+                    return false;
+                }
+            }
+            else
+            {
+                int foundDigits = 0;
+                for (var i = 0; i < digits; i++)
+                {
+                    var fromRight = trimmedInput.Length - i - 1;
+                    if (fromRight <= 0)
+                    {
+                        break;
+                    }
+                    if (trimmedInput[fromRight] is < '0' or > '9')
+                    {
+                        break;
+                    }
+
+                    foundDigits++;
+                    chars[chars.Length - 1 - i] = trimmedInput[fromRight];
+                }
+
+                if (foundDigits == 0)
+                {
+                    cleanedUp = default;
+                    return false;
+                }
+                else
+                {
+                    cleanedUp = new string(chars);
+                    return true;
+                }
+            }
+        }
+
+        public static T AbbreviationToEnumMember<T>(string name)
+            where T : Enum
+        {
+            var len = name.Length;
+            ulong val = 0;
+            for (var i = 0; i < name.Length; i++)
+            {
+                val |= (ulong)(name[i] & 0xff) << ((len - i - 1) * 8);
+            }
+            return (T)Enum.ToObject(typeof(T), val);
+        }
+    }
+}
