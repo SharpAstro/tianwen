12a22531f9f01ba86addce8afc3984c62f2224b3
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-10-08 14:42:27 +1100

    Add ASTAP and astrometry.net plate solver

---
+++ src/Astrometry/PlateSolve/AstrometryNetPlateSolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace Astap.Lib.Astrometry.PlateSolve;
+
+public class AstrometryNetPlateSolver : ExternalProcessPlateSolverBase
+{
+    public AstrometryNetPlateSolver() { }
+
+    public override string Name => "Astromentry.NET plate solver";
+
+    protected override PlatformID CommandPlatform => PlatformID.Unix;
+
+    protected override string CommandPath => "solve-field";
+
+    protected override string FormatSearchPosition((double ra, double dec)? searchOrigin, double? searchRadius)
+    {
+        if (!searchOrigin.HasValue)
+        {
+            return "";
+        }
+
+        var sb = new StringBuilder(30);
+        sb.AppendFormat("--ra {0:0.#######} --dec \"{1:0.#######}\"", searchOrigin.Value.ra, searchOrigin.Value.dec);
+        if (searchRadius is double radius)
+        {
+            sb.AppendFormat(" --radius {0.##}", radius);
+        }
+        return sb.ToString();
+    }
+
+    protected override string FormatSolveProcessArgs(string normalisedFilePath, string pixelScaleFmt, string searchPosFmt)
+        => $"\"{normalisedFilePath}\" --no-plots --overwrite {pixelScaleFmt} {searchPosFmt} -N none -U none -S none -B none -M none -R none --axy none --temp-axy";
+
+    protected override string FormatImageDimenstions(ImageDim? imageDim, float range)
+    {
+        if (imageDim is ImageDim dim)
+        {
+            var low = dim.PixelScale - range;
+            var high = dim.PixelScale + range;
+            var pixelScaleFmt = $"-u app -L {low:0.##} -H {high:0.##}";
+            return pixelScaleFmt;
+        }
+        return "";
+    }
+}
--- src/Astrometry/PlateSolve/IPlateSolver.cs
+++ src/Astrometry/PlateSolve/IPlateSolver.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Threading;
 using System.Threading.Tasks;
 
 namespace Astap.Lib.Astrometry.PlateSolve;
 
+public record struct ImageDim(
+    float PixelScale, // arcsec per pixel
+    int Width, // pixel
+    int Height /* pixel */)
+{
+    const float ArcSecToDeg = 1.0f / 60.0f * 1.0f / 60.0f;
+
+    /// <summary>
+    /// Returns field of view in degrees
+    /// </summary>
+    public (float width, float height) FieldOfView => (ArcSecToDeg * PixelScale * Width, ArcSecToDeg * PixelScale * Height);
+}
+
 public interface IPlateSolver
 {
+    internal const float DefaultRange = 0.03f;
 
+    string Name { get; }
+
+    Task<bool> CheckSupportAsync(CancellationToken cancellationToken = default);
+
+    Task<(double ra, double dec)?> SolveFileAsync(
+        string fitsFile,
+        ImageDim? imageDim = default,
+        float range = DefaultRange,
+        (double ra, double dec)? searchOrigin = default,
+        double? searchRadius = default,
+        CancellationToken cancellationToken = default
+    );
 }
