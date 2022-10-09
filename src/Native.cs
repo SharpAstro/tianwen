edf87c71af880e6009268d1d8dfcb3f42017b1b6
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-10-09 15:03:27 +1100

    Use .NET library to access ZWO asi stuff

--- src/Native.cs
+++ src/Native.cs
@@ -16,16 +16,4 @@ internal static class Native
         out double medianFWHM,
         out double background
     );
-
-    [DllImport(astap_lib, CharSet = CharSet.Unicode, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool find_asi_camera_by_index(int index, out int cameraId, out ASI_ERROR_CODE error);
-
-    [DllImport(astap_lib, CharSet = CharSet.Unicode, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool find_asi_camera_by_name(string name, out int cameraId, out ASI_ERROR_CODE error);
-
-    [DllImport(astap_lib, CharSet = CharSet.Unicode, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool connect_asi_camera(int cameraId, out ASI_ERROR_CODE error);
-
-    [DllImport(astap_lib, CharSet = CharSet.Unicode, SetLastError = false, CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool disconnect_asi_camera(int cameraId, out ASI_ERROR_CODE error);
 }
