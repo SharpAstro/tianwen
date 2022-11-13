f2e53631eb2243453ddf6104b79128b8464debc9
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-11-13 11:50:55 +1100

    Fix copy openngc script

--- src/Astap.Lib/Astrometry/Catalogs/Copy-OpenNGC.ps1
+++ src/Astap.Lib/Astrometry/Catalogs/Copy-OpenNGC.ps1
@@ -2,5 +2,5 @@
 $csvFiles = @{ NGC = 'NGC'; 'NGC.addendum' = 'addendum/addendum' }
 
 $csvFiles.GetEnumerator() | ForEach-Object {
-    $null = 7z -mx9 -scsUTF-8 a "$($_.Key).csv.gz" "$PSScriptRoot/../OpenNGC/$($_.Value).csv"
+    $null = 7z -mx9 -scsUTF-8 a "$($_.Key).csv.gz" "$PSScriptRoot/../../OpenNGC/$($_.Value).csv"
 }
