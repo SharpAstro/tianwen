19d9fd4c5719f9283cc0c02ea599963a3487600f
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-11-13 12:21:06 +1100

    Rebuild data files

--- src/Astap.Lib/Astrometry/Catalogs/IAUNamedStarDB.cs
+++ src/Astap.Lib/Astrometry/Catalogs/IAUNamedStarDB.cs
@@ -10,19 +10,6 @@ using static Astap.Lib.EnumHelper;
 
 namespace Astap.Lib.Astrometry.Catalogs;
 
-record IAUNamedStarDTO(
-    string IAUName,
-    string Designation,
-    string? ID,
-    string Constellation,
-    string? WDSComponentId,
-    string? WDS_J,
-    float? Vmag,
-    double RA_J2000, // in degrees 0..360
-    double Dec_J2000,
-    DateTime ApprovalDate
-);
-
 public class IAUNamedStarDB : ICelestialObjectDB
 {
     private readonly Dictionary<CatalogIndex, CelestialObject> _stellarObjectsByCatalogIndex = new(460);
---
+++ src/Astap.Lib/Astrometry/Catalogs/IAUNamedStarDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Astap.Lib.Astrometry.Catalogs;
+
+record IAUNamedStarDTO(
+    string IAUName,
+    string Designation,
+    string? ID,
+    string Constellation,
+    string? WDSComponentId,
+    string? WDS_J,
+    float? Vmag,
+    double RA_J2000, // in degrees 0..360
+    double Dec_J2000,
+    DateTime ApprovalDate
+);
