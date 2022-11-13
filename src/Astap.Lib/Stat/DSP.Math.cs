ef4a59160d45e028426cb7e4b485da94f37d10bd
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-11-14 10:21:20 +1100

    Add SMID functions

--- src/Astap.Lib/Stat/DSP.Math.cs
+++ src/Astap.Lib/Stat/DSP.Math.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
 
 namespace Astap.Lib.Stat;
 
@@ -15,11 +17,29 @@ public partial class DSP
         /// </summary>
         public static double[] Multiply(double[] a, double[] b)
         {
-            Debug.Assert(a.Length == b.Length, "Length of arrays a[] and b[] must match.");
+            if (a.Length != b.Length)
+            {
+                throw new ArgumentException($"{nameof(a)} and {nameof(b)} are not the same length", nameof(b));
+            }
 
-            double[] result = new double[a.Length];
-            for (uint i = 0; i < a.Length; i++)
-                result[i] = a[i] * b[i];
+            int length = a.Length;
+            double[] result = new double[length];
+
+            // Get the number of elements that can't be processed in the vector
+            // NOTE: Vector<T>.Count is a JIT time constant and will get optimized accordingly
+            int remaining = length % Vector<double>.Count;
+
+            for (int i = 0; i < length - remaining; i += Vector<double>.Count)
+            {
+                var v1 = new Vector<double>(a, i);
+                var v2 = new Vector<double>(b, i);
+                (v1 * v2).CopyTo(result, i);
+            }
+
+            for (int i = length - remaining; i < length; i++)
+            {
+                result[i] = a[i] + b[i];
+            }
 
             return result;
         }
@@ -29,9 +49,23 @@ public partial class DSP
         /// </summary>
         public static double[] Multiply(double[] a, double b)
         {
-            double[] result = new double[a.Length];
-            for (uint i = 0; i < a.Length; i++)
-                result[i] = a[i] * b;
+            int length = a.Length;
+            double[] result = new double[length];
+
+            // Get the number of elements that can't be processed in the vector
+            // NOTE: Vector<T>.Count is a JIT time constant and will get optimized accordingly
+            int remaining = length % Vector<double>.Count;
+
+            for (int i = 0; i < length - remaining; i += Vector<double>.Count)
+            {
+                var v1 = new Vector<double>(a, i);
+                (v1 * b).CopyTo(result, i);
+            }
+
+            for (int i = length - remaining; i < length; i++)
+            {
+                result[i] = a[i] + b;
+            }
 
             return result;
         }
@@ -41,11 +75,29 @@ public partial class DSP
         /// </summary>
         public static double[] Add(double[] a, double[] b)
         {
-            Debug.Assert(a.Length == b.Length, "Length of arrays a[] and b[] must match.");
+            if (a.Length != b.Length)
+            {
+                throw new ArgumentException($"{nameof(a)} and {nameof(b)} are not the same length", nameof(b));
+            }
 
-            double[] result = new double[a.Length];
-            for (uint i = 0; i < a.Length; i++)
+            int length = a.Length;
+            double[] result = new double[length];
+
+            // Get the number of elements that can't be processed in the vector
+            // NOTE: Vector<T>.Count is a JIT time constant and will get optimized accordingly
+            int remaining = length % Vector<double>.Count;
+
+            for (int i = 0; i < length - remaining; i += Vector<double>.Count)
+            {
+                var v1 = new Vector<double>(a, i);
+                var v2 = new Vector<double>(b, i);
+                (v1 + v2).CopyTo(result, i);
+            }
+
+            for (int i = length - remaining; i < length; i++)
+            {
                 result[i] = a[i] + b[i];
+            }
 
             return result;
         }
@@ -93,11 +145,29 @@ public partial class DSP
         /// </summary>
         public static double[] Divide(double[] a, double[] b)
         {
-            Debug.Assert(a.Length == b.Length, "Length of arrays a[] and b[] must match.");
+            if (a.Length != b.Length)
+            {
+                throw new ArgumentException($"{nameof(a)} and {nameof(b)} are not the same length", nameof(b));
+            }
 
-            double[] result = new double[a.Length];
-            for (uint i = 0; i < a.Length; i++)
-                result[i] = a[i] / b[i];
+            int length = a.Length;
+            double[] result = new double[length];
+
+            // Get the number of elements that can't be processed in the vector
+            // NOTE: Vector<T>.Count is a JIT time constant and will get optimized accordingly
+            int remaining = length % Vector<double>.Count;
+
+            for (int i = 0; i < length - remaining; i += Vector<double>.Count)
+            {
+                var v1 = new Vector<double>(a, i);
+                var v2 = new Vector<double>(b, i);
+                (v1 * v2).CopyTo(result, i);
+            }
+
+            for (int i = length - remaining; i < length; i++)
+            {
+                result[i] = a[i] + b[i];
+            }
 
             return result;
         }
@@ -129,14 +199,7 @@ public partial class DSP
         /// <summary>
         /// Squares a[].
         /// </summary>
-        public static double[] Square(double[] a)
-        {
-            double[] result = new double[a.Length];
-            for (uint i = 0; i < a.Length; i++)
-                result[i] = a[i] * a[i];
-
-            return result;
-        }
+        public static double[] Square(double[] a) => Multiply(a, a);
 
         /// <summary>
         /// Log10 a[].
