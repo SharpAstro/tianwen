591669403cef3c0db944b6280776c969fe0f2ac9
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-11-15 16:44:54 +1100

    More BitMatrix tests

--- src/Astap.Lib.Tests/BitMatrixTests.cs
+++ src/Astap.Lib.Tests/BitMatrixTests.cs
@@ -1,4 +1,5 @@
-﻿using Shouldly;
+﻿using CommunityToolkit.HighPerformance;
+using Shouldly;
 using System;
 using Xunit;
 
@@ -6,6 +7,46 @@ namespace Astap.Lib.Tests
 {
     public class BitMatrixTests
     {
+        [Theory]
+        [InlineData(2, 3, 3, 2)]
+        [InlineData(3, 2, 2, 3)]
+        public void GivenInvalidXYWhenIndexingThenAnExceptionIsThrown(int d0, int d1, int x, int y)
+        {
+            // given
+            var bm = new BitMatrix(d0, d1);
+
+            // when, then
+            Should.Throw<IndexOutOfRangeException>(() => bm[x, y]);
+            Should.Throw<IndexOutOfRangeException>(() => bm[x, y] = true);
+        }
+
+        [Theory]
+        [InlineData(2, 3)]
+        [InlineData(3, 2)]
+        public void GivenDimsWhenGetLengthThenItIsReturned(int d0, int d1)
+        {
+            // given
+            var bm = new BitMatrix(d0, d1);
+
+            // when
+            var d0Len = bm.GetLength(0);
+            var d1Len = bm.GetLength(1);
+
+            // then
+            d0Len.ShouldBe(d0);
+            d1Len.ShouldBe(d1);
+        }
+
+        [Fact]
+        public void GivenInvalidDimThenArgumentExceptionIsThrown()
+        {
+            // given
+            var bm = new BitMatrix(1, 1);
+
+            // when, then
+            Should.Throw<ArgumentOutOfRangeException>(() => bm.GetLength(2));
+        }
+
         [Theory]
         [InlineData(2, 32, 1, 1,
             "00000000 00000000 00000000 00000000",
@@ -35,7 +76,9 @@ namespace Astap.Lib.Tests
             bm[x, y] = true;
 
             // then
-            var actualRows = bm.ToString().Split('\n', '\r', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            var stringOutput = bm.ToString();
+            var actualRows = BitMatrixOutputAsRows(stringOutput);
+            stringOutput.Count('1').ShouldBe(1);
             actualRows.ShouldBe(expectedRows);
         }
 
@@ -66,6 +109,10 @@ namespace Astap.Lib.Tests
             "00000000 00000000 00000000 00000000, 00000000 00000000 00000000 00000000",
             "11111111 11111111 10000000 00000000, 00000000 00000000 00000011 11111111"
         )]
+        [InlineData(2, 3 * 32, 1, 13, 75,
+            "00000000 00000000 00000000 00000000, 00000000 00000000 00000000 00000000, 00000000 00000000 00000000 00000000",
+            "11111111 11111111 11100000 00000000, 11111111 11111111 11111111 11111111, 00000000 00000000 00000111 11111111"
+        )]
         public void GivenXYWhenSetUsingRangeIndexerThenBitsAreSet(int d0, int d1, int x, int y1, int y2, params string[] expectedRows)
         {
             // given
@@ -75,8 +122,60 @@ namespace Astap.Lib.Tests
             bm[x, y1..y2] = true;
 
             // then
-            var actualRows = bm.ToString().Split('\n', '\r', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            var stringOutput = bm.ToString();
+            string[] actualRows = BitMatrixOutputAsRows(stringOutput);
+            stringOutput.Count('1').ShouldBe(y2 - y1);
             actualRows.ShouldBe(expectedRows);
         }
+
+        [Theory]
+        [InlineData(2, 32, 1, 1, 2,
+             "11111111 11111111 11111111 11111111",
+             "11111111 11111111 11111111 11111101"
+        )]
+        [InlineData(2, 32, 0, 31, 32,
+             "01111111 11111111 11111111 11111111",
+             "11111111 11111111 11111111 11111111"
+        )]
+        [InlineData(1, 32, 0, 4, 5,
+             "11111111 11111111 11111111 11101111"
+        )]
+        [InlineData(1, 32, 0, 4, 7,
+             "11111111 11111111 11111111 10001111"
+        )]
+        [InlineData(2, 64, 0, 44, 45,
+             "11111111 11111111 11111111 11111111, 11111111 11111111 11101111 11111111",
+             "11111111 11111111 11111111 11111111, 11111111 11111111 11111111 11111111"
+        )]
+        [InlineData(2, 64, 1, 15, 16,
+             "11111111 11111111 11111111 11111111, 11111111 11111111 11111111 11111111",
+             "11111111 11111111 01111111 11111111, 11111111 11111111 11111111 11111111"
+        )]
+        [InlineData(2, 64, 1, 15, 42,
+             "11111111 11111111 11111111 11111111, 11111111 11111111 11111111 11111111",
+             "00000000 00000000 01111111 11111111, 11111111 11111111 11111100 00000000"
+        )]
+        [InlineData(2, 3 * 32, 1, 13, 75,
+             "11111111 11111111 11111111 11111111, 11111111 11111111 11111111 11111111, 11111111 11111111 11111111 11111111",
+             "00000000 00000000 00011111 11111111, 00000000 00000000 00000000 00000000, 11111111 11111111 11111000 00000000"
+        )]
+        public void GivenXYWhenClearUsingRangeIndexerThenBitsAreCleared(int d0, int d1, int x, int y1, int y2, params string[] expectedRows)
+        {
+            // given
+            var bm = new BitMatrix(d0, d1);
+            bm.SetAll();
+
+            // when
+            bm[x, y1..y2] = false;
+
+            // then
+            var stringOutput = bm.ToString();
+            var actualRows = BitMatrixOutputAsRows(stringOutput);
+            stringOutput.Count('0').ShouldBe(y2 - y1);
+            actualRows.ShouldBe(expectedRows);
+        }
+
+        private static string[] BitMatrixOutputAsRows(string stringOutput) =>
+            stringOutput.Split('\n', '\r', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
     }
 }
