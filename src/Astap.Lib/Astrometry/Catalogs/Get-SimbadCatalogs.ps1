109bd183e6f8233303c09ef08c8804c83865ca01
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-11-22 14:07:14 +1100

    Add Bayer cat

--- src/Astap.Lib/Astrometry/Catalogs/Get-SimbadCatalogs.ps1
+++ src/Astap.Lib/Astrometry/Catalogs/Get-SimbadCatalogs.ps1
@@ -1,20 +1,33 @@
 
+$commonFilter = "^(Barnard|RCW|LDN|GUM|SH|NAME|NGC|IC|Ced|M |HD|HR)"
 
-$catalogs = @('Sh', 'RCW', 'GUM', 'LDN', 'Ced', 'Barnard')
+$catalogs = [ordered]@{
+    Bayer = "^(HD|HR|NAME|2MASS|[*])"
+    Barnard = $commonFilter
+    RCW = $commonFilter
+    GUM = $commonFilter
+    LDN = $commonFilter
+    Ced = $commonFilter
+    Sh = $commonFilter
+}
 $outParams = "main_id,ids,otype(3),ra(d;ICRS),dec(d,ICRS),fluxdata(V)"
-$catalogs | ForEach-Object {
-    $cat = $_
+$catalogs.GetEnumerator() | ForEach-Object {
+    $cat = $_.Name
+    $filter = $_.Value
+    Write-Host "Querying catalog $($cat) using ID filter $($filter)"
     [xml]$table = Invoke-RestMethod "http://simbad.u-strasbg.fr/simbad/sim-id?output.format=votable&Ident=$($cat)&NbIdent=cat&output.params=$($outParams)"
 
     $entries = $table.VOTABLE.RESOURCE.TABLE.DATA.TABLEDATA.TR | ForEach-Object {
         if (-not [string]::IsNullOrWhiteSpace($_.TD[3]) -and -not [string]::IsNullOrWhiteSpace($_.TD[4])) {
+            $ids = $_.TD[1].Split('|') -match $filter
+
             [PSCustomObject]@{
                 MainId = $_.TD[0]
-                Ids = $_.TD[1].Split('|')
+                Ids = $ids
                 ObjType = $_.TD[2]
                 Ra = [double]$_.TD[3]
                 Dec = [double]$_.TD[4]
-                VMag = if ([string]::IsNullOrWhiteSpace($_.TD[6])) { $null } else { [double]$_.TD[6] }
+                VMag = if (-not [string]::IsNullOrWhiteSpace($_.TD[6])) { [double]$_.TD[6] } else { $null }
             }
         }
     }
