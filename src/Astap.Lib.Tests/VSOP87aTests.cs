cf92ee9d620c01fd823fb5fe419efa358acf07f5
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-11-30 07:58:59 +1100

    Use DTO parse exact

--- src/Astap.Lib.Tests/VSOP87aTests.cs
+++ src/Astap.Lib.Tests/VSOP87aTests.cs
@@ -10,12 +10,12 @@ namespace Astap.Lib.Tests;
 public class VSOP87aTests
 {
     [Theory]
-    [InlineData(CatalogIndex.Moon, "2020-04-11T16:00:00Z", 274.236400, 38.2464000, 16.61305867, -20.90403, 261.7692, -24.3259)]
-    [InlineData(CatalogIndex.Sol, "2022-11-29T10:40:00+11:00", 145.1663118d, -37.884547d, 16.3253006d, -21.43031427d, 73.1644087d, 54.3085913d)]
+    [InlineData(CatalogIndex.Moon, "2020-04-11T16:00:00.0000000Z", 274.236400, 38.2464000, 16.61305867, -20.90403, 261.7692, -24.3259)]
+    [InlineData(CatalogIndex.Sol, "2022-11-29T10:40:00.0000000+11:00", 145.1663118d, -37.884547d, 16.3253006d, -21.43031427d, 73.1644087d, 54.3085913d)]
     public void GivenPlanetAndPositionWhenReducingOrbitThenRaDecAzAltIsReturned(CatalogIndex catIdx, string dateStr, double @long, double lat, double expRa, double expDec, double expAz, double expAlt)
     {
         // given
-        var dto = DateTimeOffset.Parse(dateStr, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+        var dto = DateTimeOffset.ParseExact(dateStr, "o", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
 
         // when
         var reduced = VSOP87a.Reduce(catIdx, dto, lat, @long, out var actualRa, out var actualDec, out var actualAz, out var actualAlt);
