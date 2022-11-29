db78f6d9c8238456b3790b807e34c71b2cf70b09
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-11-29 16:56:24 +1100

    Use Catalog index for planets + Sun

--- src/Astap.Lib.Tests/VSOP87aTests.cs
+++ src/Astap.Lib.Tests/VSOP87aTests.cs
@@ -1,4 +1,5 @@
-﻿using Astap.Lib.Astrometry.VSOP87;
+﻿using Astap.Lib.Astrometry.Catalogs;
+using Astap.Lib.Astrometry.VSOP87;
 using Shouldly;
 using System;
 using System.Globalization;
@@ -9,15 +10,15 @@ namespace Astap.Lib.Tests;
 public class VSOP87aTests
 {
     [Theory]
-    [InlineData(Body.Moon, "2020-04-11T16:00:00Z", 274.236400, 38.2464000, 16.61305867, -20.90403, 261.7692, -24.3259)]
-    [InlineData(Body.Sun, "2022-11-29T10:40:00+11:00", 145.1663118d, -37.884547d, 16.3253006d, -21.43031427d, 73.1644087d, 54.3085913d)]
-    public void GivenPlanetAndPositionWhenReducingOrbitThenRaDecAzAltIsReturned(Body body, string dateStr, double @long, double lat, double expRa, double expDec, double expAz, double expAlt)
+    [InlineData(CatalogIndex.Moon, "2020-04-11T16:00:00Z", 274.236400, 38.2464000, 16.61305867, -20.90403, 261.7692, -24.3259)]
+    [InlineData(CatalogIndex.Sol, "2022-11-29T10:40:00+11:00", 145.1663118d, -37.884547d, 16.3253006d, -21.43031427d, 73.1644087d, 54.3085913d)]
+    public void GivenPlanetAndPositionWhenReducingOrbitThenRaDecAzAltIsReturned(CatalogIndex catIdx, string dateStr, double @long, double lat, double expRa, double expDec, double expAz, double expAlt)
     {
         // given
         var dto = DateTimeOffset.Parse(dateStr, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
 
         // when
-        VSOP87a.Reduce(body, dto, lat, @long, out var actualRa, out var actualDec, out var actualAz, out var actualAlt);
+        VSOP87a.Reduce(catIdx, dto, lat, @long, out var actualRa, out var actualDec, out var actualAz, out var actualAlt);
 
         // then
         actualRa.ShouldBeInRange(expRa - 0.1, expRa + 0.1);
