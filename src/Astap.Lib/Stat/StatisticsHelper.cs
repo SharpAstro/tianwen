ace6079d00a63c66eee87ee200fde8679d65aad7
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-12-23 13:07:11 +1100

    Add GCD calculation

--- src/Astap.Lib/Stat/StatisticsHelper.cs
+++ src/Astap.Lib/Stat/StatisticsHelper.cs
@@ -28,4 +28,44 @@ public static class StatisticsHelper
         int mid = values.Length / 2;
         return values.Length % 2 != 0 ? values[mid] : (values[mid] + values[mid - 1]) / 2;
     }
+
+    /// <summary>
+    /// Calculates the GCD of the concatenated list first + rest (rest is copied).
+    /// </summary>
+    /// <param name="first">first item</param>
+    /// <param name="rest">rest items</param>
+    /// <returns>GCD of all values</returns>
+    public static uint GCD(int first, params int[] rest)
+    {
+        var len = rest.Length + 1;
+        Span<int> values = len < 128 ? stackalloc int[len] : new int[len];
+
+        values[0] = first;
+        rest.AsSpan().CopyTo(values[1..]);
+
+        return GCD(values);
+    }
+
+    public static uint GCD(in Span<int> values)
+    {
+        if (values.Length > 1)
+        {
+            do
+            {
+                values.Sort((a, b) => Math.Abs(b).CompareTo(Math.Abs(a)));
+
+                if (values[1] != 0)
+                {
+                    values[0] %= values[1];
+                }
+                else
+                {
+                    return (uint)Math.Abs(values[0]);
+                }
+            }
+            while (true);
+        }
+
+        return (uint)Math.Abs(values[0]);
+    }
 }
