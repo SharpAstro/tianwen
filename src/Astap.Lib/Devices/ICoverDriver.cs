3d63ba53bf86148c042590eab16b7a6e7e219cce
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-12-30 19:46:10 +1100

    Move common code to ICoverDriver

--- src/Astap.Lib/Devices/ICoverDriver.cs
+++ src/Astap.Lib/Devices/ICoverDriver.cs
@@ -2,25 +2,53 @@
 
 public interface ICoverDriver : IDeviceDriver
 {
-    bool IsOpen { get; }
+    /// <summary>
+    /// Convenience method for <see cref="CoverState"/> == <see cref="CoverStatus.Open"/>.
+    /// </summary>
+    public bool IsOpen => CoverState == CoverStatus.Open;
 
-    bool IsClosed { get; }
+    /// <summary>
+    /// Convenience method for <see cref="CoverState"/> == <see cref="CoverStatus.Closed"/>.
+    /// </summary>
+    public bool IsClosed => CoverState == CoverStatus.Closed;
 
-    bool IsMoving { get; }
+    /// <summary>
+    /// Returns true when either closing or opening, convenience method for <see cref="CoverState"/> == <see cref="CoverStatus.Moving"/>.
+    /// </summary>
+    public bool IsMoving => CoverState == CoverStatus.Moving;
 
-    bool IsCalibrationReady { get; }
+    public bool IsCalibrationReady => IsClosed && CalibratorState is not CalibratorStatus.NotReady or CalibratorStatus.NotPresent or CalibratorStatus.Error;
 
+    /// <summary>
+    /// Returns true if cover started opening.
+    /// </summary>
+    /// <returns></returns>
     bool Open();
 
+    /// <summary>
+    /// Returns true if cover started closing.
+    /// </summary>
+    /// <returns></returns>
     bool Close();
 
     bool CalibratorOn(int brightness);
 
     bool CalibratorOff();
 
+    /// <summary>
+    /// Returns the current calibrator brightness in the range 0 (completely off) to <see cref="MaxBrightness"/> (fully on).
+    /// </summary>
     int Brightness { get; }
+
+    /// <summary>
+    /// Maximum brightness value for <see cref="CalibratorOn(int)"/>, will be -1 if unknown.
+    /// </summary>
+    int MaxBrightness { get; }
 
     CoverStatus CoverState { get; }
 
+    /// <summary>
+    /// Returns the state of the calibration device, if present, otherwise returns <see cref="CalibratorStatus.NotPresent"/>.
+    /// </summary>
     CalibratorStatus CalibratorState { get; }
 }
