59f17b6d48c932480f123755caf496c004be794e
Sebastian Godelet <sebastian.godelet@outlook.com>
2023-01-01 12:44:17 +1100

    Add profile serialisation test

--- src/Astap.Lib.Tests/ProfileTests.cs
+++ src/Astap.Lib.Tests/ProfileTests.cs
@@ -1,6 +1,9 @@
 ﻿using Astap.Lib.Devices.Builtin;
 using Shouldly;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Astap.Lib.Tests;
@@ -14,5 +17,31 @@ public class ProfileTests
         var actualUri = Profile.CreateProfileUri(new Guid(guid), name);
 
         actualUri.ToString().ShouldBe(expectedUriStr);
+    }
+
+    [Theory]
+    [InlineData("11111111-2222-3333-4444-555555555555", "Saved profile", "Key", "some:://value")]
+    public async Task GivenProfileWhenSavedAndLoadedThenItIsIdentical(string guid, string name, string key, string valueUriStr)
+    {
+        // given
+        var dir = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("D")));
+        try
+        {
+            var profile = new Profile(new Guid(guid), name, new Dictionary<string, Uri> { [key] = new Uri(valueUriStr) });
+
+            // when
+            await profile.SaveAsync(dir);
+            var actualProfiles = await Profile.LoadExistingProfilesAsync(dir);
+
+            // then
+            actualProfiles.ShouldNotBeEmpty();
+            actualProfiles.Count.ShouldBe(1);
+
+            actualProfiles[0].ShouldBeEquivalentTo(profile);
+        }
+        finally
+        {
+            dir.Delete(true);
+        }
     }
 }
