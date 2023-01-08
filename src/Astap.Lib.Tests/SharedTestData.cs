b5639b9b7150242f55a08efc8c61c143afb28528
Sebastian Godelet <sebastian.godelet@outlook.com>
2023-01-08 13:47:34 +1100

    Add Mel + Coll cats

--- src/Astap.Lib.Tests/SharedTestData.cs
+++ src/Astap.Lib.Tests/SharedTestData.cs
@@ -22,6 +22,7 @@ public static class SharedTestData
     internal const CatalogIndex C069 = (CatalogIndex)((ulong)'C' << 21 | '0' << 14 | '6' << 7 | '9');
     internal const CatalogIndex C092 = (CatalogIndex)((ulong)'C' << 21 | '0' << 14 | '9' << 7 | '2');
     internal const CatalogIndex C099 = (CatalogIndex)((ulong)'C' << 21 | '0' << 14 | '9' << 7 | '9');
+    internal const CatalogIndex Cr024 = (CatalogIndex)((ulong)'C' << 28 | 'r' << 21 | '0' << 14 | '2' << 7 | '4');
     internal const CatalogIndex Cr399 = (CatalogIndex)((ulong)'C' << 28 | 'r' << 21 | '3' << 14 | '9' << 7 | '9');
     internal const CatalogIndex Ced0014 = (CatalogIndex)((ulong)'C' << 42 | (ulong)'e' << 35 | (ulong)'d' << 28 | '0' << 21 | '0' << 14 | '1' << 7 | '4');
     internal const CatalogIndex Ced0016 = (CatalogIndex)((ulong)'C' << 42 | (ulong)'e' << 35 | (ulong)'d' << 28 | '0' << 21 | '0' << 14 | '1' << 7 | '6');
@@ -57,9 +58,11 @@ public static class SharedTestData
     internal const CatalogIndex M051 = (CatalogIndex)('M' << 21 | '0' << 14 | '5' << 7 | '1');
     internal const CatalogIndex M054 = (CatalogIndex)('M' << 21 | '0' << 14 | '5' << 7 | '4');
     internal const CatalogIndex M102 = (CatalogIndex)('M' << 21 | '1' << 14 | '0' << 7 | '2');
+    internal const CatalogIndex Mel013 = (CatalogIndex)((ulong)'M' << 35 | (ulong)'e' << 28 | 'l' << 21 | '0' << 14 | '1' << 7 | '3');
     internal const CatalogIndex Mel022 = (CatalogIndex)((ulong)'M' << 35 | (ulong)'e' << 28 | 'l' << 21 | '0' << 14 | '2' << 7 | '2');
     internal const CatalogIndex NGC0056 = (CatalogIndex)((ulong)'N' << 28 | '0' << 21 | '0' << 14 | '5' << 7 | '6');
     internal const CatalogIndex NGC0526_B = (CatalogIndex)((ulong)'N' << 42 | (ulong)'0' << 35 | (ulong)'5' << 28 | '2' << 21 | '6' << 14 | '_' << 7 | 'B');
+    internal const CatalogIndex NGC0869 = (CatalogIndex)((ulong)'N' << 28 | '0' << 21 | '8' << 14 | '6' << 7 | '9');
     internal const CatalogIndex NGC1530_A = (CatalogIndex)((ulong)'N' << 42 | (ulong)'1' << 35 | (ulong)'5' << 28 | '3' << 21 | '0' << 14 | '_' << 7 | 'A');
     internal const CatalogIndex NGC1333 = (CatalogIndex)((ulong)'N' << 28 | '1' << 21 | '3' << 14 | '3' << 7 | '3');
     internal const CatalogIndex NGC1976 = (CatalogIndex)((ulong)'N' << 28 | '1' << 21 | '9' << 14 | '7' << 7 | '6');
--- src/Astap.Lib/Astrometry/Catalogs/Get-SimbadCatalogs.ps1
+++ src/Astap.Lib/Astrometry/Catalogs/Get-SimbadCatalogs.ps1
@@ -1,10 +1,11 @@
 [CmdLetBinding()]
 param(
     [string] $Cat = $null,
-    [string] $Filter = '.*'
+    [string] $Filter = '.*',
+    [switch] $SkipStarCats = $false
 )
 
-$commonFilter = "^(Barnard|RCW|LDN|GUM|SH|NAME|NGC|IC|Ced|CG |M |HD|HR|VDB|HH|Dobashi|DG )"
+$commonFilter = "^(Barnard|RCW|LDN|GUM|SH|NAME|NGC|IC|Ced|CG |M |HD|HR|VDB|HH|Dobashi|DG |Cl\s+(Collinder|Melotte))"
 $starCatFilter = '^(HD|HR|NAME|2MASS|[*]|M |(NGC|IC)\s+\d+[A-Za-z]?$)'
 
 if ([string]::IsNullOrWhiteSpace($Cat)) {
@@ -33,25 +34,28 @@ $outParams = "main_id,ids,otype(3),ra(d;ICRS),dec(d,ICRS),fluxdata(V)"
 $catalogs.GetEnumerator() | ForEach-Object {
     $cat = $_.Name
     $filter = $_.Value
-    Write-Host "Querying catalog $($cat) using ID filter $($filter)"
-    [xml]$table = Invoke-RestMethod "http://simbad.u-strasbg.fr/simbad/sim-id?output.format=votable&Ident=$($cat)&NbIdent=cat&output.params=$($outParams)"
+    if ($filter -eq $starCatFilter -and $SkipStarCats) {
+        Write-Host "Skipping star catalog $cat"
+    } else {
+        Write-Host "Querying catalog $($cat) using ID filter $($filter)"
+        [xml]$table = Invoke-RestMethod "http://simbad.u-strasbg.fr/simbad/sim-id?output.format=votable&Ident=$($cat)&NbIdent=cat&output.params=$($outParams)"
 
-    $entries = $table.VOTABLE.RESOURCE.TABLE.DATA.TABLEDATA.TR | ForEach-Object {
-        if (-not [string]::IsNullOrWhiteSpace($_.TD[3]) -and -not [string]::IsNullOrWhiteSpace($_.TD[4])) {
-            $ids = $_.TD[1].Split('|') -match $filter
-
-            [PSCustomObject]@{
-                MainId = $_.TD[0]
-                Ids = $ids
-                ObjType = $_.TD[2]
-                Ra = [double]$_.TD[3]
-                Dec = [double]$_.TD[4]
-                VMag = if (-not [string]::IsNullOrWhiteSpace($_.TD[6])) { [double]$_.TD[6] } else { $null }
+        $entries = $table.VOTABLE.RESOURCE.TABLE.DATA.TABLEDATA.TR | ForEach-Object {
+            if (-not [string]::IsNullOrWhiteSpace($_.TD[3]) -and -not [string]::IsNullOrWhiteSpace($_.TD[4])) {
+                $ids = $_.TD[1].Split('|') -match $filter
+                [PSCustomObject]@{
+                    MainId = $_.TD[0]
+                    Ids = $ids
+                    ObjType = $_.TD[2]
+                    Ra = [double]$_.TD[3]
+                    Dec = [double]$_.TD[4]
+                    VMag = if (-not [string]::IsNullOrWhiteSpace($_.TD[6])) { [double]$_.TD[6] } else { $null }
+                }
             }
         }
-    }
 
-    $outFile = "$PSScriptRoot/$($cat).json"
-    $entries | ConvertTo-Json | Out-File -Encoding UTF8NoBOM $outFile
-    $null = 7z -mx9 -scsUTF-8 a "$($outFile).gz" $outFile
+        $outFile = "$PSScriptRoot/$($cat).json"
+        $entries | ConvertTo-Json | Out-File -Encoding UTF8NoBOM $outFile
+        $null = 7z -mx9 -scsUTF-8 a "$($outFile).gz" $outFile
+    }
 }
