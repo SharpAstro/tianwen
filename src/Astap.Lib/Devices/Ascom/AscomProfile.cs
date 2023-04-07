1abd4c4155acc24d42728de9edc23f922c5020a4
Sebastian Godelet <sebastian.godelet@outlook.com>
2023-04-07 15:44:35 +1000

    Enable Dynamic object override for non-windows systems

--- src/Astap.Lib/Devices/Ascom/AscomProfile.cs
+++ src/Astap.Lib/Devices/Ascom/AscomProfile.cs
@@ -14,8 +14,8 @@ public class AscomProfile : DynamicComObject, IDeviceSource<AscomDevice>
 
     public IEnumerable<string> RegisteredDeviceTypes => EnumerateProperty<string>(_comObject?.RegisteredDeviceTypes);
 
-    IEnumerable<(string key, string value)> RegisteredDevicesKV(string deviceType) => EnumerateKeyValueProperty(_comObject?.RegisteredDevices(deviceType));
+    IEnumerable<KeyValuePair<string, string>> RegisteredDevicesKV(string deviceType) => EnumerateKeyValueProperty(_comObject?.RegisteredDevices(deviceType));
 
     public IEnumerable<AscomDevice> RegisteredDevices(string deviceType)
-        => RegisteredDevicesKV(deviceType).Select(p => new AscomDevice(deviceType, p.key, p.value));
+        => RegisteredDevicesKV(deviceType).Select(p => new AscomDevice(deviceType, p.Key, p.Value));
 }
--- src/Astap.Lib/DynamicComObject.cs
+++ src/Astap.Lib/DynamicComObject.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
@@ -6,16 +7,47 @@ namespace Astap.Lib;
 
 public class DynamicComObject : IDisposable
 {
-    public static object? NewComObject(string progId) =>
-        RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && Type.GetTypeFromProgID(progId) is Type type
-            ? Activator.CreateInstance(type)
-            : null;
+    private static readonly ConcurrentDictionary<string, Func<string, object?>> _initaliserMap = new();
+    private static readonly ConcurrentDictionary<string, Type?> _progIdTypeCache = new();
+
+    /// <summary>
+    /// Register an <paramref name="initialiser"/> for a given <paramref name="progId"/>.
+    /// </summary>
+    /// <param name="progId"></param>
+    /// <param name="initialiser"></param>
+    public static void RegisterTypeCreator(string progId, Func<string, object?> initialiser)
+        => _initaliserMap.AddOrUpdate(progId, initialiser, (_, _) => initialiser);
+
+    private static Type? TryGetTypeFromProgID(string progId)
+        => RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && Type.GetTypeFromProgID(progId) is { } type ? type : null;
+
+    /// <summary>
+    /// Uses <paramref name="progId"/> to lookup a registered initialiser that was previously registered with <see cref="RegisterTypeCreator(string, Func{string, object?})"/>.
+    /// If none is found and the current platform is <see cref="OSPlatform.Windows"/> and a ProgID mapping exists then that is used to create an instance.
+    /// </summary>
+    /// <param name="progId"></param>
+    /// <returns></returns>
+    public static object? CreateInstanceFromProgId(string progId)
+    {
+        if (_initaliserMap.TryGetValue(progId, out var initialiser))
+        {
+            return initialiser(progId);
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && _progIdTypeCache.GetOrAdd(progId, TryGetTypeFromProgID) is { } type)
+        {
+            return Activator.CreateInstance(type);
+        }
+        else
+        {
+            return null;
+        }
+    }
 
     protected readonly dynamic? _comObject;
 
     private bool _disposedValue;
 
-    public DynamicComObject(string progId) => _comObject = NewComObject(progId);
+    public DynamicComObject(string progId) => _comObject = CreateInstanceFromProgId(progId);
 
     public static IEnumerable<T> EnumerateProperty<T>(dynamic property)
     {
@@ -30,7 +62,7 @@ public class DynamicComObject : IDisposable
         }
     }
 
-    public static IEnumerable<(string key, string value)> EnumerateKeyValueProperty(dynamic property)
+    public static IEnumerable<KeyValuePair<string, string>> EnumerateKeyValueProperty(dynamic property)
     {
         if (property is null)
         {
@@ -41,7 +73,7 @@ public class DynamicComObject : IDisposable
         {
             if (item?.Key is string key && item.Value is string value)
             {
-                yield return (key, value);
+                yield return new(key, value);
             }
         }
     }
