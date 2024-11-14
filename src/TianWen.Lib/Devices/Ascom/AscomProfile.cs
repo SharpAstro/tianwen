6fc9ae8f262f3aea46bc57d2d6fb972c77181ef6
Sebastian Godelet <sebastian.godelet@outlook.com>
2024-11-15 09:41:32 +1100

    Use ASCOM for .NET standard

--- src/TianWen.Lib/Devices/Ascom/AscomProfile.cs
+++ src/TianWen.Lib/Devices/Ascom/AscomProfile.cs
@@ -1,28 +1,49 @@
-﻿using System;
+﻿using ASCOM.Com;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AscomProfile = ASCOM.Com.Profile;
+using AscomDeviceType = ASCOM.Common.DeviceTypes;
 
 namespace TianWen.Lib.Devices.Ascom;
 
-internal class AscomProfile : DynamicComObject, IDeviceSource<AscomDevice>
+internal class AscomDeviceIterator : IDeviceSource<AscomDevice>
 {
-    public AscomProfile() : base("ASCOM.Utilities.Profile") { }
+    private static readonly bool IsSupported = PlatformUtilities.IsPlatformInstalled();
 
     /// <summary>
     /// Returns true if COM object was initalised successfully.
     /// </summary>
-    public ValueTask<bool> CheckSupportAsync(CancellationToken cancellationToken = default) => ValueTask.FromResult(_comObject is not null);
+    public ValueTask<bool> CheckSupportAsync(CancellationToken cancellationToken = default) => ValueTask.FromResult(IsSupported);
 
-    public ValueTask DiscoverAsync(CancellationToken cancellationToken = default) => ValueTask.CompletedTask;
+    private static readonly HashSet<AscomDeviceType> _allSupportedDeviceTypes = [AscomDeviceType.Camera, AscomDeviceType.FilterWheel, AscomDeviceType.Focuser, AscomDeviceType.Telescope];
 
-    public IEnumerable<DeviceType> RegisteredDeviceTypes => RegisteredDeviceTypesInternal.Select(DeviceTypeHelper.TryParseDeviceType);
+    private Dictionary<DeviceType, List<AscomDevice>> _devices = [];
 
-    private IEnumerable<string> RegisteredDeviceTypesInternal => EnumerateProperty<string>(_comObject?.RegisteredDeviceTypes);
+    public IEnumerable<DeviceType> RegisteredDeviceTypes => IsSupported ? _allSupportedDeviceTypes.Select(d => d.ToDeviceType()) : [];
 
-    private IEnumerable<KeyValuePair<string, string>> RegisteredDevicesKV(DeviceType deviceType) => EnumerateKeyValueProperty(_comObject?.RegisteredDevices(deviceType.ToString()));
+    public async ValueTask DiscoverAsync(CancellationToken cancellationToken = default)
+    {
+        if (await CheckSupportAsync(cancellationToken))
+        {
+            var devices = new Dictionary<DeviceType, List<AscomDevice>>();
+            foreach (var ascomDeviceType in _allSupportedDeviceTypes)
+            {
+                var drivers = AscomProfile.GetDrivers(ascomDeviceType);
+                var deviceType = ascomDeviceType.ToDeviceType();
+                var devicesOfType = new List<AscomDevice>();
+                foreach (var driver in drivers)
+                {
+                    devicesOfType.Add(new AscomDevice(deviceType, driver.ProgID, driver.Name));
+                }
 
-    public IEnumerable<AscomDevice> RegisteredDevices(DeviceType deviceType)
-        => RegisteredDevicesKV(deviceType).Select(p => new AscomDevice(deviceType, p.Key, p.Value));
+                devices[deviceType] = devicesOfType;
+            }
+
+            Interlocked.Exchange(ref _devices, devices);
+        }
+    }
+
+    public IEnumerable<AscomDevice> RegisteredDevices(DeviceType deviceType) => _devices[deviceType];
 }
