9be85b33e1e24f0a87c5c6d9e68d2c6391d1e918
Sebastian Godelet <sebastian.godelet@outlook.com>
2025-12-26 15:55:18 +1100

    NINA focus result tests

--- src/TianWen.Lib.Tests/FindVCurveFromFitsFileTests.cs
+++ src/TianWen.Lib.Tests/FindVCurveFromFitsFileTests.cs
@@ -1,5 +1,9 @@
 ﻿using Shouldly;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using TianWen.Lib.Astrometry.Focus;
 using TianWen.Lib.Stat;
@@ -10,11 +14,70 @@ namespace TianWen.Lib.Tests;
 
 public class FindVCurveFromFitsFileTests(ITestOutputHelper testOutputHelper) : ImageAnalyserTests(testOutputHelper)
 {
+    record NinaTestResult(
+        int Version,
+        NinaSamplingMethod Method,
+        NinaFittingMethod Fitting,
+        FocusPoint CalculatedFocusPoint,
+        FocusPoint PreviousFocusPoint,
+        IReadOnlyList<FocusPoint> MeasurePoints,
+        IReadOnlyDictionary<string, string> Fittings
+    );
+
+    enum NinaSamplingMethod
+    {
+        Unknown,
+        StarHFR
+    }
+
+    enum NinaFittingMethod
+    {
+        Unknown,
+        Hyperbolic
+    }
+    record FocusPoint(double Position, double Value, double Error);
+
+    [Theory]
+    [InlineData("2025-12-17--23-06-56--9d0e769c-5847-470c-a25e-0e1de367e31e.json")]
+    [InlineData("2025-12-17--23-27-02--9d0e769c-5847-470c-a25e-0e1de367e31e.json")]
+    [InlineData("2025-12-18--01-36-05--9d0e769c-5847-470c-a25e-0e1de367e31e.json")]
+    [InlineData("2025-12-18--02-26-57--9d0e769c-5847-470c-a25e-0e1de367e31e.json")]
+    public async Task ReachSameResultFromSuccessfulNINARun(string file)
+    {
+        var ninaResult = await JsonSerializer.DeserializeAsync<NinaTestResult>(SharedTestData.OpenEmbeddedFileStream(file).ShouldNotBeNull(), new JsonSerializerOptions
+        {
+            Converters = { new JsonStringEnumConverter() }
+        });
+        ninaResult.ShouldNotBeNull();
+        ninaResult.Fitting.ShouldBe(NinaFittingMethod.Hyperbolic);
+        ninaResult.CalculatedFocusPoint.ShouldNotBeNull();
+        ninaResult.MeasurePoints.ShouldNotBeEmpty();
+
+        var ninaFitting = ninaResult.Fittings[ninaResult.Fitting.ToString()];
+
+        var sampleKind = ninaResult.Method switch
+        {
+            NinaSamplingMethod.StarHFR => SampleKind.HFD,
+            var x => throw new InvalidOperationException($"Sampling method {x} is not supported")
+        };
+
+        var sampleMap = new MetricSampleMap(sampleKind, AggregationMethod.Average);
+
+        foreach (var point in ninaResult.MeasurePoints)
+        {
+            sampleMap.AddSampleAtFocusPosition((int)Math.Round(point.Position), (float)point.Value);
+        }
+        sampleMap.TryGetBestFocusSolution(out var solution, out var minPos, out var maxPos).ShouldBeTrue();
+    
+        ninaResult.CalculatedFocusPoint.Position.ShouldBeInRange(minPos, maxPos);
+        solution.ShouldNotBeNull().P.ShouldBeInRange(minPos, maxPos);
+    }
+
     [Theory]
     [InlineData(SampleKind.HFD, AggregationMethod.Average, 28208, 28211, 1, 1, 1, 10f, 20, 2, 130)]
     [InlineData(SampleKind.HFD, AggregationMethod.Average, 28227, 28231, 1, 1, 1, 10f, 20, 2, 140)]
     [InlineData(SampleKind.HFD, AggregationMethod.Average, 28208, 28231, 1, 1, 1, 10f, 20, 2, 130)]
-    public async Task GivenFocusSamplesWhenSolvingAHyperboleIsFound(SampleKind kind, AggregationMethod aggregationMethod, int focusStart, int focusEndIncl, int focusStepSize, int sampleCount, int filterNo, float snrMin, int maxIterations, int expectedSolutionAfterSteps, int expectedMinStarCount)
+    public async Task HyperboleIsFoundFromActualImageRun(SampleKind kind, AggregationMethod aggregationMethod, int focusStart, int focusEndIncl, int focusStepSize, int sampleCount, int filterNo, float snrMin, int maxIterations, int expectedSolutionAfterSteps, int expectedMinStarCount)
     {
         // given
         var sampleMap = new MetricSampleMap(kind, aggregationMethod);
