553a8e0b4b5c8b408567d7f6059d936e578866fd
Sebastian Godelet <sebastian.godelet@outlook.com>
2026-01-07 11:52:08 +1100

    Refactor sub commands

---
+++ src/TianWen.Lib.CLI/ParseResultExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.CommandLine;
+using TianWen.Lib.Devices;
+
+namespace TianWen.Lib.CLI;
+
+static class ParseResultExtensions
+{
+    internal static Profile? GetSelected(this ParseResult parseResult, IReadOnlyCollection<Profile> allProfiles, Option<string?> selectedProfileOption)
+    {
+        var selectedOptionValue = parseResult.GetValue(selectedProfileOption);
+        if (Guid.TryParse(selectedOptionValue, out var selectedId))
+        {
+            return allProfiles.SingleOrDefault(p => p.ProfileId == selectedId);
+        }
+        else if (allProfiles.Count is 1 && string.IsNullOrEmpty(selectedOptionValue))
+        {
+            return allProfiles.Single();
+        }
+        else
+        {
+            var possibleProfiles = allProfiles.Where(p => string.Equals(p.DisplayName, selectedOptionValue, StringComparison.CurrentCultureIgnoreCase)).ToList();
+
+            return possibleProfiles.Count is 1 ? possibleProfiles.Single() : null;
+        }
+    }
+}
